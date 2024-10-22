83104660530ce5e6e7508285b4d6d69b464378a6
Esteban <ricardoespace@gmail.com>
2024-10-22 11:55:45 -0600

    Se eligio una plantilla nueva para el proyecto

--- DjangoApi/urls.py
+++ DjangoApi/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.login.login_view import login_view
+from api.login.login_view import login_view, register_view, forgot_view
 from api.home.home_view import home_view
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('login/', login_view, name = "login"),
-    path('', home_view, name = "home") 
+    path('register/', register_view, name='register'),
+    path('forgot/', forgot_view, name='forgot'),
+    path('', home_view, name = "home"),
 ]
--- api/login/login_view.py
+++ api/login/login_view.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 def login_view(request):
-    template_view = "auth-login.html"
+    template_view = "sign-in.html"
+
+    return render(request,template_view)
+
+def register_view(request):
+    template_view = "sign-up.html"
+
+    return render(request,template_view)
+
+def forgot_view(request):
+    template_view = "auth-forgot-password.html"
 
     return render(request,template_view)
